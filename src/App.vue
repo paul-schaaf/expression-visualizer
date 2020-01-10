9fd51cb919c21168bf3d3c86286c098e9fa28aa4
Paul Schaaf <paul.schaaf@maibornwolff.de>
2020-01-10 15:14:21 +0100

    App.vue now displays squares based on kvData

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,49 @@
                 color="primary"
                 dark
         >
+            <v-spacer/>
+            <v-text-field placeholder="Expression goes here!"/>
+            <v-btn text large>Draw KV Map</v-btn>
+            <v-spacer/>
         </v-app-bar>
+        <v-content>
+            <v-container>
+                <v-row
+                        v-for="(row, rowIndex) in kvData"
+                        :key="'kvRow' + rowIndex"
+                        align="center"
+                        justify="center"
+                >
+                    <v-sheet
+                            v-for="(square, columnIndex) in row"
+                            :key="'kvRow' + rowIndex + columnIndex"
+                            :class="{'invisible': !kvData[rowIndex][columnIndex]}"
+                            color="light-blue lighten-3"
+                            height="20"
+                            width="20"
+                            tile
+                            class="ma-1"
+                    />
+                </v-row>
+
+            </v-container>
+        </v-content>
     </v-app>
 </template>
 
 <script>
     export default {
         name: 'App',
+        data() {
+            return {
+                kvData: [[true, true, false, true], [true, false, true, true]]
+            };
+        }
     };
 </script>
+
+<style lang="css">
+    .invisible {
+        opacity: 0
+    }
+</style>
