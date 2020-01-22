87bd3ae731348f581b394965a1cef08e07942d81
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-01-22 20:00:15 +0100

    Improved Folder Organisation

--- src/KVCalculation/index.js
+++ src/KVCalculation/index.js
@@ -1,31 +1,44 @@
 const Parser = require('./Parser');
 const Lexer = require('./Lexer');
-const KVNodeFactory = require('./KVNodeFactory');
-const MapConfigCreator = require('./MapConfigCreator');
+const KVNodeFactory = require('./KVMap/KVNodeFactory');
+const MapConfigCreator = require('./KVMap/MapConfigCreator');
 
 const nonVariables = require('./enums/nonVariables');
 
-export default function getKVArray (input) {
-    const fileContent = input.trim();
-    if (!fileContent) throw 'Please enter an expression';
-    const lexer = new Lexer(fileContent);
-    const variables = [];
-    while (lexer.hasNext()) {
-        const nextToken = lexer.next();
-        if(!variables.includes(nextToken) && !nonVariables.includes(nextToken))
-            variables.push(nextToken);
-    }
-    variables.sort();
-    const mapConfigs = MapConfigCreator.createMapConfigs(variables.length);
-
+export default function getKVArray(input) {
+    const lexer = createLexer(input);
+    const root = parse(lexer);
     lexer.reset();
-    const parser = new Parser(lexer);
-    const root = parser.parse();
+
+    const variables = getVariables(lexer);
+    const mapConfigs = MapConfigCreator.createMapConfigs(variables.length);
     const KVRoot = convertToKVTree(root, mapConfigs, variables);
     return KVRoot.execute().toArray();
 }
 
-function convertToKVTree(node, mapConfigs, variables){
+function createLexer(input) {
+    const fileContent = input.trim();
+    if (!fileContent) throw 'Please enter an expression';
+    return new Lexer(fileContent);
+}
+
+function parse(lexer) {
+    const parser = new Parser(lexer);
+    return parser.parse();
+}
+
+function getVariables(lexer) {
+    const variables = [];
+    while (lexer.hasNext()) {
+        const nextToken = lexer.next();
+        if (!variables.includes(nextToken) && !nonVariables.includes(nextToken))
+            variables.push(nextToken);
+    }
+    variables.sort();
+    return variables;
+}
+
+function convertToKVTree(node, mapConfigs, variables) {
     const kvNode = KVNodeFactory.createNode(node, mapConfigs, variables);
     const childrenKVNodes = node.getChildren().map(c => convertToKVTree(c, mapConfigs, variables));
     childrenKVNodes.forEach(c => kvNode.addChild(c));
